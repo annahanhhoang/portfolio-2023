e360900b8f5ff0ea0fc25f10cc6fc4e2a8c8cb51
Anna Hoang <anna.hoang@rewardstyle.com>
2023-03-28 11:53:06 -0500

    :recycle:refactor: Move nav drawer and footer to home page

--- src/App.vue
+++ src/App.vue
@@ -1,133 +1,26 @@
 <template>
   <v-app>
-    <v-app-bar v-if="mdAndDown" position="fixed" class="no-print">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
-      <social-link class="mr-4" :xl="true"></social-link>
-    </v-app-bar>
-
-    <!--
-    **
-    **   SIDE MENU
-    **
-    -->
-    <v-navigation-drawer v-model="drawer" class="app-side-menu">
-      <v-list>
-        <v-list-item class="text-center">
-          <v-img class="mt-4" src="@/assets/logo.jpg" width="100%" />
-
-          <p class="text-h4">Anna Hoang</p>
-
-          <p class="menu-position">Full-stack Developer</p>
-        </v-list-item>
-      </v-list>
-
-      <v-divider />
-
-      <v-list nav :lines="false">
-        <v-list-item v-for="item in menu" :key="item.title" :target="item.target" :to="item.to">
-          <template v-slot:prepend>
-            <v-icon :icon="item.icon"></v-icon>
-          </template>
-
-          <v-list-item-title>
-            {{ item.title }}
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
-      <template v-slot:append>
-        <social-link v-if="mdAndUp" class="mb-6"></social-link>
-      </template>
-    </v-navigation-drawer>
-
-    <!--
-        **
-        **   MAIN CONTENT
-        **
-    -->
     <v-main>
       <router-view />
     </v-main>
-
-    <v-footer
-      v-if="route.name !== 'resume'"
-      app
-      height="auto"
-      class="justify-center align-center no-print"
-    >
-      <v-card class="flex" flat rounded="0">
-        <v-card-actions class="justify-center">
-          <p>&copy;{{ currentYear }} - <strong>Anna Hoang</strong></p>
-        </v-card-actions>
-      </v-card>
-    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent, watch } from 'vue';
 import { useRoute } from 'vue-router';
-import { useDisplay } from 'vuetify';
-
-import SocialLink from '@/components/SocialLink.vue';
 
 export default defineComponent({
-  components: {
-    SocialLink,
-  },
   setup() {
     const route = useRoute();
-
-    const drawer = ref(false);
-    const menu = [
-      { title: 'Home', icon: 'mdi-home', to: '/home' },
-      {
-        title: 'About Me',
-        icon: 'mdi-account',
-        to: '/home#about-section',
-        target: '_self',
-      },
-      {
-        title: 'Experience',
-        icon: 'mdi-briefcase',
-        to: '/home#experience-section',
-        target: '_self',
-      },
-      {
-        title: 'Skills and Educations',
-        icon: 'mdi-school',
-        to: '/home#skill-section',
-        target: '_self',
-      },
-      {
-        title: 'My Projects',
-        icon: 'mdi-lightbulb-on',
-        to: '/home#projects-section',
-        target: '_self',
-      },
-      {
-        title: 'Resume',
-        icon: 'mdi-file',
-        to: '/resume',
-        target: '_blank',
-      },
-    ];
-    const currentYear = new Date().getFullYear();
-
-    const { mdAndDown, mdAndUp } = useDisplay();
 
     watch(
       route,
       () => {
         toggleScroll();
-        toggleDrawer();
       },
       { deep: true, immediate: true }
     );
-
-    function toggleDrawer() {
-      drawer.value = route.name !== 'landing' && route.name !== 'resume';
-    }
 
     // disable scrolling for landing page
     function toggleScroll() {
@@ -142,14 +35,8 @@ export default defineComponent({
     }
 
     return {
-      drawer,
-      menu,
-      currentYear,
       route,
-      mdAndDown,
-      mdAndUp,
       toggleScroll,
-      toggleDrawer,
     };
   },
 });
@@ -174,9 +61,5 @@ a {
   .no-print * {
     display: none !important;
   }
-}
-
-.v-list-item--active > .v-list-item__overlay {
-  opacity: 0 !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,84 @@
 <template>
-  <v-container class="home-page">
-    <about id="about-section" class="my-4" />
-    <experience id="experience-section" class="mb-4" />
-    <skill id="skill-section" class="mb-4" />
-    <projects id="projects-section" class="mb-4" />
-  </v-container>
+  <div>
+    <v-app-bar v-if="mdAndDown" position="fixed" class="no-print">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <social-link class="mr-4" :xl="true"></social-link>
+    </v-app-bar>
+
+    <!--
+      **
+      **   SIDE MENU
+      **
+      -->
+    <v-navigation-drawer v-model="drawer" class="app-side-menu">
+      <v-list>
+        <v-list-item class="text-center">
+          <v-img class="mt-4" src="@/assets/logo.jpg" width="100%" />
+
+          <p class="text-h4">Anna Hoang</p>
+
+          <p class="menu-position">Full-stack Developer</p>
+        </v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list nav :lines="false">
+        <v-list-item v-for="item in menu" :key="item.title" :target="item.target" :to="item.to">
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+
+          <v-list-item-title>
+            {{ item.title }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <social-link v-if="mdAndUp" class="mb-6"></social-link>
+      </template>
+    </v-navigation-drawer>
+
+    <!--
+        **
+        **   MAIN CONTENT
+        **
+    -->
+    <v-container class="home-page">
+      <about id="about-section" class="my-4" />
+      <experience id="experience-section" class="mb-4" />
+      <skill id="skill-section" class="mb-4" />
+      <projects id="projects-section" class="mb-4" />
+    </v-container>
+
+    <!--
+        **
+        **   FOOTER
+        **
+    -->
+
+    <v-footer app height="auto" class="justify-center align-center no-print">
+      <v-card class="flex" flat rounded="0">
+        <v-card-actions class="justify-center">
+          <p>&copy;{{ currentYear }} - <strong>Anna Hoang</strong></p>
+        </v-card-actions>
+      </v-card>
+    </v-footer>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { useDisplay } from 'vuetify';
 
 import about from '@/views/About.vue';
 import experience from '@/views/Experience.vue';
 import skill from '@/views/Skills.vue';
 import projects from '@/views/Projects.vue';
+
+import SocialLink from '@/components/SocialLink.vue';
 
 export default defineComponent({
   components: {
@@ -21,6 +86,63 @@ export default defineComponent({
     experience,
     skill,
     projects,
+    SocialLink,
+  },
+
+  setup() {
+    const route = useRoute();
+
+    const drawer = ref(true);
+    const menu = [
+      { title: 'Home', icon: 'mdi-home', to: '/home' },
+      {
+        title: 'About Me',
+        icon: 'mdi-account',
+        to: '/home#about-section',
+        target: '_self',
+      },
+      {
+        title: 'Experience',
+        icon: 'mdi-briefcase',
+        to: '/home#experience-section',
+        target: '_self',
+      },
+      {
+        title: 'Skills and Educations',
+        icon: 'mdi-school',
+        to: '/home#skill-section',
+        target: '_self',
+      },
+      {
+        title: 'My Projects',
+        icon: 'mdi-lightbulb-on',
+        to: '/home#projects-section',
+        target: '_self',
+      },
+      {
+        title: 'Resume',
+        icon: 'mdi-file',
+        to: '/resume',
+        target: '_blank',
+      },
+    ];
+    const currentYear = new Date().getFullYear();
+
+    const { mdAndDown, mdAndUp } = useDisplay();
+
+    return {
+      currentYear,
+      drawer,
+      menu,
+      mdAndDown,
+      mdAndUp,
+    };
   },
 });
 </script>
+
+<style lang="scss">
+.v-list-item--active > .v-list-item__overlay {
+  opacity: 0 !important;
+}
+</style>
